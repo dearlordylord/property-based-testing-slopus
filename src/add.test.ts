ea923e5b509a5dfe34296b31fe6e99e0fb15a887
Igor Loskutov <igor.loskutoff@gmail.com>
2026-06-14 18:16:21 -0700

    Remove explicit example tests from add test suite

--- src/add.test.ts
+++ src/add.test.ts
@@ -5,14 +5,6 @@ import fc from "fast-check";
 import { add } from "./add";
 
 describe("add", () => {
-  it("returns 3 for 1+2", () => {
-    assert.equal(add(1, 2), 3);
-  });
-
-  it("returns 4 for 2+2", () => {
-    assert.equal(add(2, 2), 4);
-  });
-
   it("is commutative", () => {
     fc.assert(
       fc.property(fc.integer(), fc.integer(), (a, b) => {
